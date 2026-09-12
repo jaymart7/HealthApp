9fd5511edd9a99ae966840581c5d17b8b73fbab2
Jaymart <jaymart.b.araga@gmail.com>
2026-09-12 10:43:07 +0800

    Bump version code to 7 and version name to 1.7; update OAuth client configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "ph.mart.healthapp"
         minSdk = 24
         targetSdk = 37
-        versionCode = 6
-        versionName = "1.5"
+        versionCode = 7
+        versionName = "1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
