e0c34fa1b6147c035ea643641ae45b1831e1cdfc
Jaymart <jaymart.b.araga@gmail.com>
2026-09-19 11:17:20 +0800

    Bumped version code to 15 and updated version name to 1.15

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "ph.mart.healthapp"
         minSdk = 24
         targetSdk = 37
-        versionCode = 14
-        versionName = "1.14"
+        versionCode = 15
+        versionName = "1.15"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
