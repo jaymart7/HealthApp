220cc98cf270ab13c79a70973eb9f6ce73d02d0c
Jaymart <jaymart.b.araga@gmail.com>
2026-09-22 11:47:59 +0800

    bump version to 1.18

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "ph.mart.healthapp"
         minSdk = 24
         targetSdk = 37
-        versionCode = 17
-        versionName = "1.17"
+        versionCode = 18
+        versionName = "1.18"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
